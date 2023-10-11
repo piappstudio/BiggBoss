2f167909f3b4f4292f55dbfb537d1dea7c67e938
Pi App Studio <70383910+piappstudio@users.noreply.github.com>
2023-10-11 17:04:44 -0500

    Version 1.0

--- shared/src/commonMain/kotlin/ui/detail/ShowDetailScreen.kt
+++ shared/src/commonMain/kotlin/ui/detail/ShowDetailScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -178,7 +179,7 @@ class ShowDetailScreen(private val title: String, val url:String) : Screen {
     @Composable
     fun RenderContestantRow(participant: ParticipantItem, votingOption: VotingOption) {
         val navigator = LocalNavigator.currentOrThrow
-        ElevatedCard  (modifier = Modifier.piShadow().clickable {
+        Surface  (modifier = Modifier.piShadow().clickable {
             val json = Json { ignoreUnknownKeys = true }
             val jsonParticipantItem = json.encodeToString(participant)
             val jsonVotingOption = json.encodeToString(votingOption)
